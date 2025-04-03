39c0538b509f252752c4e8ed636abdd47f22482d
Mahir <mresidovic1@etf.unsa.ba>
2025-04-03 15:20:25 +0200

    feature(refactoring)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@src": "/src",
+      "@assets": "/src/assets",
+      "@fonts": "/src/assets/fonts",
+      "@icons": "/src/assets/icons",
+      "@images": "/src/assets/images",
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@data": "/src/data",
+      "@hooks": "/src/hooks",
+      "@routes": "/src/routes",
+      "@sections": "/src/sections",
+      "@styles": "/src/styles",
+      "@utils": "/src/utils",
+      "@store": "/src/store",
+      "@services": "/src/services",
+    }, 
+  },
 })
