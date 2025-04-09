e247393f15a51e891b9f767d1f8e717de4fb6c3d
edii <edelmo1@etf.unsa.ba>
2025-04-09 22:57:05 +0200

    Modal za editovanje prodavnice

---
+++ src/components/EditStoreModalStyle.jsx
@@ -0,0 +1,20 @@
+const styles = {
+    modalBox: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      backgroundColor: 'white',
+      padding: 3,
+      minWidth: 400,
+      boxShadow: 24,
+      borderRadius: 2,
+    },
+    buttonsContainer: {
+      display: 'flex',
+      justifyContent: 'space-between',
+     marginTop: 2,
+    },
+  };
+
+  export default styles;
