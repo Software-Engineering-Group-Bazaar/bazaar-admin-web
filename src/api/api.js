2e0f9f077af1bea7cd0525fc2527427d29d98385
mbjelobrk1 <148221673+mbjelobrk1@users.noreply.github.com>
2025-04-15 22:58:19 +0200

    Feature/mockapi (#68)
    * Mock
    
    * Mock cijeli fale jos produkti
    
    * Gotov mock
    
    * Zaboravio

--- src/api/api.js
+++ src/api/api.js
@@ -3,6 +3,9 @@ import apiClientInstance from './apiClientInstance';
 import TestAuthApi from './api/TestAuthApi';
 import LoginDTO from './model/LoginDTO';
 import users from '../data/users';
+import stores from '../data/stores';
+import categories from '../data/categories';
+import products from '../data/products';
 import pendingUsers from '../data/pendingUsers.js';
 import axios from 'axios';
 
@@ -132,13 +135,16 @@ export const apiCreateUserAsync = async (newUserPayload) => {
     try {
       // dodaj novog korisnika u niz "users"
       const newUser = {
-        ...newUserPayload,
+        email: newUserPayload.email,
+        userName: newUserPayload.userName,
+        password: newUserPayload.password,
         id: users.length + 1,
         isApproved: false,
+        roles:[newUserPayload.role]
       };
       //users.push(newUser);
       pendingUsers.push(newUser);
-      return newUser;
+      return {data: newUser};
     } catch (error) {
       console.error('Greška pri kreiranju korisnika:', error);
       throw error;
@@ -265,7 +271,37 @@ export const apiDeleteUserAsync = async (userId) => {
 // }
 
 export const apiCreateProductAsync = async (productData) => {
-  if (API_ENV_DEV !== API_FLAG) {
+  if(API_ENV_DEV == API_FLAG){
+    try {
+      const formData = new FormData();
+      formData.append('RetailPrice', String(productData.price ?? 0));
+      formData.append(
+        'ProductCategoryId',
+        String(productData.productcategoryid)
+      );
+      formData.append('WholesalePrice', String(productData.price ?? 0));
+      formData.append('Name', productData.name);
+      formData.append('Weight', String(productData.weight ?? 0));
+      formData.append('Volume', String(productData.volume ?? 0));
+      formData.append('WeightUnit', productData.weightunit ?? ''); 
+      formData.append('StoreId', String(productData.storeId)); 
+      formData.append('VolumeUnit', productData.volumeunit ?? ''); 
+      const imageFiles = productData.photos;
+      if (imageFiles && imageFiles.length > 0) {
+        imageFiles.forEach((file) => {
+          if (file instanceof File) {
+            formData.append('Files', file, file.name);
+          }
+        });
+      }
+      products.push(formData);
+      return { success: true };
+    } catch (error) {
+      console.error('Product creation failed:', error);
+      return { success: false };
+    }
+  }
+  else {
     try {
       // Create a FormData object
       const formData = new FormData();
@@ -315,7 +351,7 @@ export const apiCreateProductAsync = async (productData) => {
 
 export const apiGetProductCategoriesAsync = async () => {
   if (API_ENV_DEV == API_FLAG) {
-    return mockCategories.filter((cat) => cat.type === 'product');
+    return categories.filter((cat) => cat.type === 'product');
   } else {
     apiSetAuthHeader();
     const res = await axios.get(`${baseApiUrl}/api/Admin/categories`);
@@ -325,7 +361,7 @@ export const apiGetProductCategoriesAsync = async () => {
 
 export const apiGetStoreCategoriesAsync = async () => {
   if (API_ENV_DEV == API_FLAG) {
-    return mockCategories.filter((cat) => cat.type === 'store');
+    return categories.filter((cat) => cat.type === 'store');
   } else {
     apiSetAuthHeader();
     const res = await axios.get(`${baseApiUrl}/api/Admin/store/categories`);
@@ -346,7 +382,7 @@ export const apiGetStoreByIdAsync = async (storeId) => {
       products: [],
     };
 
-    return new Promise((resolve) => setTimeout(() => resolve(mockStore), 500));
+    return stores.filter((trgovina) => trgovina.id === storeId);
   } else {
     apiSetAuthHeader();
     const store = await axios.get(`${baseApiUrl}/api/Admin/stores/${storeId}`);
@@ -356,6 +392,10 @@ export const apiGetStoreByIdAsync = async (storeId) => {
 
 export const apiUpdateStoreAsync = async (store) => {
   if (API_ENV_DEV === API_FLAG) {
+    const index = stores.indexOf((st) => store.name==st.name)
+    stores[index]={
+      ...store
+    }
     return new Promise((resolve) =>
       setTimeout(() => resolve({ success: true, data: store }), 500)
     );
@@ -376,124 +416,8 @@ export const apiUpdateStoreAsync = async (store) => {
 export const apiGetAllStoresAsync = async () => {
   if (API_ENV_DEV == API_FLAG) {
     //izbrisati poslije
-    const mockStores = [
-      {
-        id: 1,
-        name: 'Nova Market',
-        description: 'Brza i kvalitetna dostava proizvoda.',
-        address: 'Sarajevo',
-      },
-      {
-        id: 2,
-        name: 'Tech World',
-        description: 'Elektronika i gadgeti.',
-        address: 'Mostar',
-      },
-      {
-        id: 3,
-        name: 'BioShop',
-        description: 'Prirodna kozmetika i hrana.',
-        address: 'Banja Luka',
-      },
-      {
-        id: 4,
-        name: 'Fashion Spot',
-        description: 'Savremena garderoba.',
-        address: 'Tuzla',
-      },
-      {
-        id: 5,
-        name: 'Office Plus',
-        description: 'Kancelarijski materijal i oprema.',
-        address: 'Sarajevo',
-      },
-      {
-        id: 6,
-        name: 'Auto Centar',
-        description: 'Dijelovi i oprema za automobile.',
-        address: 'Zenica',
-      },
-      {
-        id: 7,
-        name: 'Pet Planet',
-        description: 'Hrana i oprema za kućne ljubimce.',
-        address: 'Mostar',
-      },
-      {
-        id: 8,
-        name: 'Green Garden',
-        description: 'Sve za vašu baštu.',
-        address: 'Sarajevo',
-      },
-      {
-        id: 9,
-        name: 'Kids Toys',
-        description: 'Igračke i oprema za djecu.',
-        address: 'Sarajevo',
-      },
-      {
-        id: 10,
-        name: 'Mega Market',
-        description: 'Vaš svakodnevni supermarket.',
-        address: 'Banja Luka',
-      },
-      {
-        id: 11,
-        name: 'Green Garden',
-        description: 'Sve za vašu baštu.',
-        address: 'Sarajevo',
-      },
-      {
-        id: 12,
-        name: 'Kids Toys',
-        description: 'Igračke i oprema za djecu.',
-        address: 'Mostar',
-      },
-      {
-        id: 13,
-        name: 'Mega Market',
-        description: 'Vaš svakodnevni supermarket.',
-        address: 'Zenica',
-      },
-      {
-        id: 14,
-        name: 'Green Garden',
-        description: 'Sve za vašu baštu.',
-        address: 'Sarajevo',
-      },
-      {
-        id: 15,
-        name: 'Kids Toys',
-        description: 'Igračke i oprema za djecu.',
-        address: 'Tuzla',
-      },
-      {
-        id: 16,
-        name: 'Mega Market',
-        description: 'Vaš svakodnevni supermarket.',
-        address: 'Banja Luka',
-      },
-      {
-        id: 17,
-        name: 'Green Garden',
-        description: 'Sve za vašu baštu.',
-        address: 'Sarajevo',
-      },
-      {
-        id: 18,
-        name: 'Kids Toys',
-        description: 'Igračke i oprema za djecu.',
-        address: 'Mostar',
-      },
-      {
-        id: 19,
-        name: 'Mega Market',
-        description: 'Vaš svakodnevni supermarket.',
-        address: 'Tuzla',
-      },
-    ];
-
-    return new Promise((resolve) => setTimeout(() => resolve(mockStores), 500));
+    return stores
+    //return new Promise((resolve) => setTimeout(() => resolve({stores}), 500));
   } else {
     apiSetAuthHeader();
     const stores = await axios.get(`${baseApiUrl}/api/Admin/stores`);
@@ -505,6 +429,12 @@ export const apiGetAllStoresAsync = async () => {
 // DELETE product category
 export const apiDeleteProductCategoryAsync = async (categoryId) => {
   if (API_ENV_DEV === API_FLAG) {
+    const rez = categories.filter((cat) => cat.id == categoryId);
+    const index = categories.indexOf(rez);
+    if(index>-1){
+      categories.splice(index,1);
+      console.log("deleted");
+    }
     return new Promise((resolve) =>
       setTimeout(() => resolve({ success: true, deletedId: categoryId }), 500)
     );
@@ -517,6 +447,12 @@ export const apiDeleteProductCategoryAsync = async (categoryId) => {
 // DELETE store category
 export const apiDeleteStoreCategoryAsync = async (categoryId) => {
   if (API_ENV_DEV === API_FLAG) {
+    const rez = categories.filter((cat) => cat.id == categoryId);
+    const index = categories.indexOf(rez);
+    if(index>-1){
+      categories.splice(index,1);
+      console.log("deleted");
+    }
     return new Promise((resolve) =>
       setTimeout(() => resolve({ success: true, deletedId: categoryId }), 500)
     );
@@ -528,12 +464,24 @@ export const apiDeleteStoreCategoryAsync = async (categoryId) => {
 
 export const apiAddProductCategoryAsync = async (name) => {
   if (API_ENV_DEV === API_FLAG) {
-    return new Promise((resolve) =>
-      setTimeout(
-        () => resolve({ success: true, data: { id: Date.now(), name } }),
-        500
-      )
-    );
+    try{
+    const newCategory = {
+      id: categories.length+1,
+      name: name,
+      type: "product",
+    }
+    categories.push(newCategory);
+    return {data: newCategory};
+  } catch(error){
+    console.log("Error pri kreiranju kategorije proizvoda!");
+    throw error;
+  }
+    //return new Promise((resolve) =>
+      //setTimeout(
+        //() => resolve({ success: true, data: { id: Date.now(), name } }),
+        //500
+      //)
+    //);
   } else {
     apiSetAuthHeader();
     try {
@@ -550,12 +498,24 @@ export const apiAddProductCategoryAsync = async (name) => {
 
 export const apiAddStoreCategoryAsync = async (name) => {
   if (API_ENV_DEV === API_FLAG) {
-    return new Promise((resolve) =>
-      setTimeout(
-        () => resolve({ success: true, data: { id: Date.now(), name } }),
-        500
-      )
-    );
+    try{
+      const newCategory = {
+        id: categories.length+1,
+        name: name,
+        type: "store",
+      }
+      categories.push(newCategory);
+      return newCategory;
+    } catch(error){
+      console.log("Error pri kreiranju kategorije trgovine!");
+      throw error;
+    }
+   // return new Promise((resolve) =>
+   //   setTimeout(
+   //     () => resolve({ success: true, data: { id: Date.now(), name } }),
+   //     500
+   //   )
+    //);
   } else {
     apiSetAuthHeader();
     try {
@@ -572,6 +532,13 @@ export const apiAddStoreCategoryAsync = async (name) => {
 };
 
 export const apiUpdateProductCategoryAsync = async (updatedCategory) => {
+  if (API_ENV_DEV === API_FLAG){
+    const index = categories.findIndex((cat) => cat.id === updatedCategory);
+    categories[index]={
+      ...updatedCategory
+    };
+    //???
+  }
   apiSetAuthHeader();
   try {
     const response = await axios.put(
@@ -586,6 +553,13 @@ export const apiUpdateProductCategoryAsync = async (updatedCategory) => {
 };
 
 export const apiUpdateStoreCategoryAsync = async (updatedCategory) => {
+  if (API_ENV_DEV === API_FLAG){
+    const index = categories.findIndex((cat) => cat.name === updatedCategory.name);
+    categories[index]={
+      ...updatedCategory
+    };
+    return { success: true, data: response.data };
+  } else {
   apiSetAuthHeader();
   try {
     const response = await axios.put(
@@ -597,10 +571,12 @@ export const apiUpdateStoreCategoryAsync = async (updatedCategory) => {
     console.error('Error updating store category:', error);
     return { success: false, message: error.message };
   }
+}
 };
 
 export const apiAddStoreAsync = async (newStore) => {
   if (API_ENV_DEV === API_FLAG) {
+    stores.push(newStore);
     return new Promise((resolve) =>
       setTimeout(
         () => resolve({ success: true, data: { ...newStore, id: Date.now() } }),
@@ -630,6 +606,14 @@ export const apiAddStoreAsync = async (newStore) => {
 
 export const apiDeleteStoreAsync = async (storeId) => {
   if (API_ENV_DEV === API_FLAG) {
+    const rez = stores.find((store) => store.id == storeId);
+    const index = stores.indexOf(rez);
+    //console.log(index);
+    if(index>-1){
+      stores.splice(index,1);
+      console.log(storeId)
+      console.log(rez.id)
+    }
     return new Promise((resolve) =>
       setTimeout(() => resolve({ success: true, deletedId: storeId }), 500)
     );
@@ -650,6 +634,10 @@ export const apiDeleteStoreAsync = async (storeId) => {
 // Mock ažuriranje korisnika
 export const apiUpdateUserAsync = async (updatedUser) => {
   if (API_ENV_DEV == API_FLAG) {
+    const index = users.findIndex((us) => us.id === updatedUser.id);
+    users[index]={
+      ...updatedUser
+    };
     return new Promise((resolve) =>
       setTimeout(() => resolve({ success: true, updatedUser }), 500)
     );
@@ -659,7 +647,7 @@ export const apiUpdateUserAsync = async (updatedUser) => {
       userName: updatedUser.email,
       id: updatedUser.id,
       role: updatedUser.roles[0],
-      isActive: updatedUser.isActive,
+      lastActive: updatedUser.isActive,
       isApproved: updatedUser.isApproved,
       email: updatedUser.email,
     });
@@ -670,6 +658,8 @@ export const apiUpdateUserAsync = async (updatedUser) => {
 export const apiToggleUserAvailabilityAsync = async (userId, currentStatus) => {
   if (API_ENV_DEV == API_FLAG) {
     const newStatus = currentStatus === 'Online' ? 'false' : 'true';
+    const index = users.find((us) => us.id==userId);
+    users[index].availability = newStatus; 
     return new Promise((resolve) =>
       setTimeout(
         () => resolve({ success: true, availability: newStatus }),
