db8a69c57d02725b56fede977b564fd7fc996a2d
mbjelobrk1 <148221673+mbjelobrk1@users.noreply.github.com>
2025-05-16 23:05:41 +0200

    Kartica za rute i konfirmacija napravljeni i uvezani (#149)

---
+++ src/components/RouteCard.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import mapa from '@images/routing-pointa-ppointb.png';
+import DeleteConfirmationModal from './DeleteRouteConfirmation';
+const RouteCard = ({route, onViewDetails, onDelete}) => {
+
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [detailsOpen, setDetailsOpen] = useState(false);
+
+  const handleDelete = async() => {
+    try{
+     await onDelete(route.id);
+    }catch(err){
+      console.log("Delete unsuccessful");
+    }finally {
+      setDeleteOpen(false);
+    }
+  }
+  const handleViewDetails = () => {
+    try{
+     onViewDetails(route.id);
+    }catch(err){
+      console.log("Failed to open route details");
+    }
+  }
+  return (
+    <Box
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundImage: `url(${mapa})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        position: 'relative',
+        borderRadius: 2,
+        overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        p: 2,
+      }}
+    >
+      {/* Top-center text */}
+      <Typography
+        variant="subtitle1"
+        sx={{
+          position: 'absolute',
+          top: 8,
+          left: '50%',
+          transform: 'translateX(-50%)',
+          color: 'white',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          px: 1.5,
+          py: 0.5,
+          borderRadius: 1,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          marginTop: 13
+        }}
+      >
+        Ruta
+      </Typography>
+
+      {/* Buttons */}
+      <Box sx={{ display: 'flex', gap: 1 }}>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          onClick={handleViewDetails}
+          sx={{ flex: 1 }}
+        >
+          Details
+        </Button>
+        <Button
+          variant="contained"
+          size="small"
+          onClick={() => setDeleteOpen(true)}
+          sx={{
+            flex: 1,
+            backgroundColor: 'white',
+            color: 'black',
+            '&:hover': {
+              backgroundColor: '#f0f0f0',
+            },
+          }}
+        >
+          Delete
+        </Button>
+      </Box>
+      <DeleteConfirmationModal
+        open={deleteOpen}
+        onClose={() => setDeleteOpen(false)}
+        onConfirm={handleDelete}
+      />
+    </Box>
+    
+  );
+};
+
+export default RouteCard;
